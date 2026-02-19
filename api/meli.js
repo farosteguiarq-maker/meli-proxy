7b7204025dc141d6f6e92e28699022f5c55a4657
farosteguiarq-maker <farosteguiarq@gmail.com>
2026-02-19 20:08:19 -0300

    Create meli.js

---
+++ api/meli.js
@@ -0,0 +1,23 @@
+api/meli.js
+export default async function handler(req, res) {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "Missing url parameter" });
+  }
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json"
+      }
+    });
+
+    const data = await response.text();
+    res.status(response.status).send(data);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
